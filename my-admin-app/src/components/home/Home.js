5fd33f7db0f4429528de3cee3f342894ba1b2519
河儿 <60065901+jay-pro@users.noreply.github.com>
2021-08-15 16:30:53 +0700

    Chart -> .jsx

--- my-admin-app/src/components/home/Home.js
+++ my-admin-app/src/components/home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./home.css";
 import FeaturedInfo from "../featuredInfo/FeaturedInfo";
+import Chart from "../chart/Chart";
 
 function Home() {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart />
     </div>
   );
 }
